33e05da10555bc19bb432b3127863f43ec93d7bb
IIIphr <sepehr.ebi.1380@gmail.com>
2020-12-02 10:17:06 +0330

    Now we have a package !

--- Core/packages/Sepehr/Shamsi_Calendar/src/routes.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-Route::get('/date','Sepehr\Shamsi_Calendar\CalendarController@get_date');
--- src/CalendarServiceProvider.php
+++ src/CalendarServiceProvider.php
@@ -23,6 +23,6 @@ class CalendarServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        include __DIR__.'/routes.php';
+        //
     }
 }
